17ac61658a103eebff9b27ff753a90d915e2d08d
Dave Sag <davesag@gmail.com>
2019-05-19 17:39:24 +0530

    n/a updated stryker

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,10 +1,9 @@
 module.exports = function(config) {
   config.set({
-    maxConcurrentTestRunners: 2, // workaround for https://github.com/stryker-mutator/stryker/issues/1525
     mutate: ['src/**/*.js', '!src/checks/check*.js', '!src/mockModels.js'],
     mutator: 'javascript',
     packageManager: 'npm',
-    reporters: ['clear-text', 'progress'],
+    reporters: ['clear-text'],
     testRunner: 'mocha',
     mochaOptions: {
       spec: ['./test/unit/**/*.test.js'],
