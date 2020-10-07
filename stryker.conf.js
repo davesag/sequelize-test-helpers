8ea35db7564f451b5ebddddd8c5216584f82e6e4
Dave Sag <davesag@gmail.com>
2020-10-08 09:43:15 +1100

    n/a updated stryker and other dependencies

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,17 +1,13 @@
-module.exports = function (config) {
-  config.set({
-    mutate: ['src/**/*.js', '!src/checks/check*.js', '!src/mockModels.js'],
-    mutator: 'javascript',
-    packageManager: 'npm',
-    reporters: ['clear-text'],
-    testRunner: 'mocha',
-    mochaOptions: {
-      spec: ['./test/unit/**/*.test.js'],
-      require: ['./test/unitTestHelper.js']
-    },
-    transpilers: [],
-    testFramework: 'mocha',
-    coverageAnalysis: 'perTest',
-    thresholds: { high: 80, low: 70, break: null }
-  })
+module.exports = {
+  // concurrency: 2,
+  mutate: ['src/**/*.js', '!src/checks/check*.js', '!src/mockModels.js'],
+  packageManager: 'npm',
+  reporters: ['clear-text'],
+  testRunner: 'mocha',
+  mochaOptions: {
+    spec: ['./test/unit/**/*.test.js'],
+    require: ['./test/unitTestHelper.js']
+  },
+  coverageAnalysis: 'perTest',
+  thresholds: { high: 80, low: 70, break: null }
 }
