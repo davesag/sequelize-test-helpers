f7d8917101d91012d3de3160d3c0bb9ef6ab33c4
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2021-04-05 14:49:42 -0400

    add Model association methods as spys to mockSequelize class

--- src/mockSequelize.js
+++ src/mockSequelize.js
@@ -4,5 +4,9 @@ const DataTypes = require('./dataTypes')
 
 class Model {}
 Model.init = spy()
+Model.belongsToMany = spy()
+Model.belongsTo = spy()
+Model.hasMany = spy()
+Model.hasOne = spy()
 
 module.exports = { Model, DataTypes }
