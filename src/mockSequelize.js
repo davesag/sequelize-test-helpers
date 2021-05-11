e8b5650fe5607d72b99cddc36dba978ff15d3f2a
Howard Edwards <howarde.edwards@gmail.com>
2021-05-11 07:37:40 -0500

    adjusted mockSequelize for jest support

--- src/mockSequelize.js
+++ src/mockSequelize.js
@@ -1,12 +1,13 @@
-const { spy } = require('sinon')
+const { isJestRunner, sinon } = require('./utils/checkIsJestRunner')
+const { spy } = sinon
 
 const DataTypes = require('./dataTypes')
 
 class Model {}
-Model.init = spy()
-Model.belongsToMany = spy()
-Model.belongsTo = spy()
-Model.hasMany = spy()
-Model.hasOne = spy()
+Model.init = isJestRunner ? jest.fn() : spy()
+Model.belongsToMany = isJestRunner ? jest.fn() : spy()
+Model.belongsTo = isJestRunner ? jest.fn() : spy()
+Model.hasMany = isJestRunner ? jest.fn() : spy()
+Model.hasOne = isJestRunner ? jest.fn() : spy()
 
 module.exports = { Model, DataTypes }
